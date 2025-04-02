3f218ee9340f1a4ed1179606072c1e42ca36ea8b
NgThiThanhNhu <nguyenthithanhnhu163tp@gmail.com>
2025-04-02 10:03:37 +0700

    updateApp.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BookCategory from "./pages/bookCategory";
-
-
-
+import LoginPage from "./pages/authentication/Login";
 import './App.css';
+import BookChapter from "./pages/bookChapter";
+
+
+
+
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Trang chủ</h1>}/>
+  
+        <Route path="/" element={<LoginPage/>}/>
         <Route path="/book-category" element={<BookCategory/>}/>
- 
+        <Route path="/book-chapter" element={<BookChapter/>}/>
         {/*Thêm các route khác nếu cấn*/ }
       </Routes>
     </Router>
